4f2808dc87328cab2ee7eb3789b9dff21ec5abed
tingSHI <ting.shi2015@gmail.com>
2024-05-26 13:33:51 +0200

    Setup Project with: - Spring (Lombok, Spring Web) - Frontend

---
+++ src/main/java/com/github/tingshi2015/d23recap4todapp2ndtry/D23Recap4ToDApp2ndTryApplication.java
@@ -0,0 +1,13 @@
+package com.github.tingshi2015.d23recap4todapp2ndtry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D23Recap4ToDApp2ndTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(D23Recap4ToDApp2ndTryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/tingshi2015/d23recap4todapp2ndtry/D23Recap4ToDApp2ndTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tingshi2015.d23recap4todapp2ndtry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D23Recap4ToDApp2ndTryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
